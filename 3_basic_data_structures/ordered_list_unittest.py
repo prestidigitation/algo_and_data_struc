55cd3f0841ba8b96fa6d65c76869f5cfc2c1ea55
prestidigitation <alexander.c.rowland@gmail.com>
2015-12-09 23:55:08 -0500

    Added unittest for ordered list add method

--- 3_basic_data_structures/ordered_list_unittest.py
+++ 3_basic_data_structures/ordered_list_unittest.py
@@ -6,16 +6,35 @@ class TestOrderedList(unittest.TestCase):
 
     def set_up(self):
         self.my_list = OrderedList()
-        self.my_list.add(31)
-        self.my_list.add(77)
-        self.my_list.add(17)
-        self.my_lits.add(93)
-        self.my_list.add(26)
-        self.my_list.add(54)
+        # self.my_list.add(31)
+        # self.my_list.add(77)
+        # self.my_list.add(17)
+        # self.my_list.add(93)
+        # self.my_list.add(26)
+        # self.my_list.add(54)
+
+        self.values = [(1, 31),
+                       (2, 77),
+                       (3, 17),
+                       (4, 93),
+                       (5, 26),
+                       (6, 54)]
 
     def test_is_empty(self):
         new_list = OrderedList()
         self.assertTrue(new_list.is_empty())
+
+    def test_add(self):
+        pass
+
+    def test_size(self):
+        new_list = OrderedList()
+        size_counter = 0
+        self.assertEqual(0, new_list.size())
+        for num, value in self.values:
+            new_list.add(value)
+            size_counter += 1
+            self.assertEqual(size_counter, new_list.size())
 
     # self.assertEqual(True, False)
 
